662ce3b02b6d27fbfd190db11fabb3de86b691a1
gunjicmilos <gunjicm.milos99@gmail.com>
2023-03-06 23:25:39 +0100

    implementingShoppingCartFunctilanity

--- BoolkyBook/Areas/Customer/Controllers/CartController.cs
+++ BoolkyBook/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,111 @@
+using System.Security.Claims;
+using BoolkyBook.Models.ViewModels;
+using BoolkyBook.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoolkyBook.Controllers;
 
 [Area("Customer")]
+[Authorize]
 public class CartController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+    public ShoppingCartVM ShoppingCartVM { get; set; }
+    public int OrderTotal { get; set; }
+    public CartController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
     // GET
     public IActionResult Index()
     {
-        return View();
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst((ClaimTypes.NameIdentifier));
+
+        ShoppingCartVM = new ShoppingCartVM()
+        {
+            ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                includeProperties:"Product")
+        };
+        foreach (var cart in ShoppingCartVM.ListCart)
+        {
+            cart.Price = GetPriceByQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
+                cart.Product.Price100);
+            ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+        }
+        return View(ShoppingCartVM);
     }
-}
+    
+    public IActionResult Summary()
+    { 
+        /*var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst((ClaimTypes.NameIdentifier));
+
+        ShoppingCartVM = new ShoppingCartVM()
+        {
+            ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                includeProperties:"Product")
+        };
+        foreach (var cart in ShoppingCartVM.ListCart)
+        {
+            cart.Price = GetPriceByQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
+                cart.Product.Price100);
+            ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+        }
+        return View(ShoppingCartVM);*/
+        return View();
+
+    }
+
+    public IActionResult Plus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.IncrementCount(cart,1);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    
+    public IActionResult Minus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        if (cart.Count <= 0)
+        {
+            _unitOfWork.ShoppingCart.Remove(cart);
+        }
+        else
+        {
+            _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+        }
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    
+    public IActionResult Remove(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.Remove(cart);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private double GetPriceByQuantity(double quantity, double price, double price50, double price100)
+    {
+        if (quantity < 50)
+        {
+            return price;
+        }
+        else
+        {
+            if (quantity < 100)
+            {
+                return price50;
+            }
+            else
+            {
+                return price100;
+            }
+        }
+    }
+} 
--- BoolkyBook/Repository/Repository.cs
+++ BoolkyBook/Repository/Repository.cs
@@ -17,7 +17,7 @@ public class Repository<T> : IRepository<T> where T : class
         this.dbSet = _db.Set<T>();
     }
     
-    public T GetFirstOrDefault(Expression<Func<T, bool>> filter,string? includeProperties = null)
+    public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null,string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
         query = query.Where(filter);
@@ -31,9 +31,14 @@ public class Repository<T> : IRepository<T> where T : class
         return query.FirstOrDefault();
     }
 
-    public IEnumerable<T> GetAll(string? includeProperties = null)
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter,string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        
         if (includeProperties != null)
         {
             foreach(var includeProp in includeProperties.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries))
